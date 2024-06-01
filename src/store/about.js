c3afefaffd5e2882a7e61808d6c3c93dbcbd65db
hojeong26 <rkdghwjd0926@jnu.ac.kr>
2024-06-01 21:53:34 +0900

    Add Serverless Functions

--- src/store/about.js
+++ src/store/about.js
@@ -3,7 +3,7 @@ import { Store } from "../core/heropy"
 export default new Store({
     photo: 'https://heropy.blog/css/images/logo.png',
     name: 'HOJEONG / KangHoJeong',
-    email:'rkdghwjd0926@jnu.ac.kr',
+    email:'hojeong@mail.com',
     blog: 'https://heropy.blog',
     github:'https://github.com/hojeong26',
     repository: 'https://github.com/hojeong26'
